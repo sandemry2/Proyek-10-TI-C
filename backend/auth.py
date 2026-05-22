9e1e7d28e056a38cc9c1cc651f25dbe3a70c042b
sandemry2 <sande.marya7245@student.unri.ac.id>
2026-05-22 14:15:45 +0700

    Update auth.py

--- backend/auth.py
+++ backend/auth.py
@@ -1,1 +1,26 @@
+from backend.file_handler import load_users
 
+def login():
+
+    users = load_users()
+
+    print("=" * 40)
+    print("LOGIN SISTEM EKOSISTEM")
+    print("=" * 40)
+
+    username = input("Username : ")
+    password = input("Password : ")
+
+    for user in users:
+
+        if (
+            user["username"] == username
+            and
+            user["password"] == password
+        ):
+
+            print("\nLogin berhasil!")
+            return True
+
+    print("\nLogin gagal!")
+    return False
